93a8cc68f852129b96baddf1b954a0300a76b5e1
xueqi <xueqi@zhongdonglinnoMac.local>
2018-09-04 17:56:00 +0800

    today文件更新

---
+++ today.js
@@ -0,0 +1,17 @@
+/**
+ *  同步一些设置 2018-09-04 17:55:25 同步公司文件
+ *  
+ *  
+ * 
+    1.  redux 学习资料合集： https://redux.js.org/introduction/ecosystem
+    2. redex 英文文档： https://redux.js.org/introduction
+    3. redux 中文文档： https://cn.redux.js.org/
+    4. 阮一峰redux 教程：
+    http://www.ruanyifeng.com/blog/2016/09/redux_tutorial_part_one_basic_usages.html
+    下载redux 的GitHub的源码： 里面包含了一些demo
+    git clone https://github.com/reactjs/redux.git 
+    redux 源码： https://unpkg.com/redux@4.0.0/
+    https://unpkg.com/redux@4.0.0/dist/redux.js
+
+
+ */
