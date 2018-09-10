5218768e4dbb25e630cbf25126bc3c98a73a6659
zhongdonglin <zhongdonglin@yuntutv.net>
2018-09-10 23:25:48 +0800

    redux更新

--- today.js
+++ today.js
@@ -12,6 +12,8 @@
     git clone https://github.com/reactjs/redux.git 
     redux 源码： https://unpkg.com/redux@4.0.0/
     https://unpkg.com/redux@4.0.0/dist/redux.js
+    比较强的：
+    https://github.com/ecmadao/Coding-Guide/blob/master/Notes/React/Redux/Redux%E5%85%A5%E5%9D%91%E8%BF%9B%E9%98%B6-%E6%BA%90%E7%A0%81%E8%A7%A3%E6%9E%90.md
    
   9.05：
   JavaScript的不可变集合
