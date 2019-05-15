32719e67fd4cec12e975c2351a495178c0703bc1
itboos <zhongdonling@gmail.com>
2019-05-15 17:10:26 +0800

    统计代码常规统计分类

--- base/al.js
+++ base/al.js
@@ -1,9 +1,7 @@
 /* eslint-disable one-var */
-/* eslint-disable2 */
 /*
  * onError -> 拦截 throw 等抛出的错误， 较为严重的错误。
  * console.error -> 脚本里的输出错误
- *  111
  *  错误信息的处理, 错误信息很长的时候限制长度
  *
  *
@@ -21,13 +19,11 @@ var smConfig = {
   reportBase: 'https://pingtas.qq.com/pingd',
   version: '0.1',
   prefix: '_sma_',
+  showLog: false,
+  autoTrack: true,
   errorReport: true, // 是否开启错误上报
   requestReport: true,
-  statShareApp: true, // 分享
-  statPullDown: true,
-  statPageScroll: true,
   maxReportNum: 1 // 当次上报最大条数
-  // stat_reach_bottom: !1
 }
 const eventMap = {
   pageView: 'pageView', //  页面停留时间
@@ -77,6 +73,7 @@ const smaUtils = {
     })
   },
   setBasicInfo: function() {
+    // 目前小程序在cookie里带上了系统信息，所以无需再次获取
     // let baseInfo = smaUtils.getSystemInfo()
     let baseInfo = {}
     smaUtils.getNetworkType(function (res) {
@@ -99,7 +96,7 @@ const smaUtils = {
       data: sendData,
       method: 'POST',
       success: (res) => {
-        console.log('success....', res)
+        smaUtils.logInfo('success....', res)
         if (res.statusCode === 200) {
           // 删除已经发送成功的数据
           if (type === 'trackData') {
@@ -108,7 +105,7 @@ const smaUtils = {
             SMA.Data.errorData.splice(0, smConfig.maxReportNum)
           }
         } else {
-          console.log('http get error:', res)
+          smaUtils.logInfo('http get error:', res)
         }
       }
     })
@@ -119,6 +116,17 @@ const smaUtils = {
       1001: 'SMA is already defined, Fail to start!'
     }
     ERRORMAP[errno] && console.warn(['Error(SMA):', ERRORMAP[errno], '(CODE' + errno + ')'].join(' '))
+  },
+  logInfo: function() {
+    if (smConfig.showLog) {
+      if (typeof console === 'object' && console.log) {
+        try {
+          return console.log.apply(console, arguments)
+        } catch (e) {
+          console.log(arguments[0])
+        }
+      }
+    }
   },
   composeData: function(type, data) {
     return {
@@ -133,211 +141,221 @@ const smaUtils = {
   composeErrorData: function(type, data) {}
 }
 
-function rePlacePage() {
-  /* global Page */
-  let oPage = Page
-  // eslint-disable-next-line
-  Page = function () {
-    let args = arguments
-    // console.log('args:', args)
-    let oOnShow = args[0].onShow,
-      oOnHide = args[0].onHide,
-      beginTime = 0,
-      currRoute = ''
-      // 重写onShow
-    args[0].onShow = function () {
-      console.log('拦截的onShow.....')
-      beginTime = smaUtils.getTime()
-      oOnShow && oOnShow.call(this, arguments)
-    }
-     // 重写onHide
-    args[0].onHide = function () {
-      console.log('拦截的onHide....')
-      currRoute = smaUtils.getPagePath()
-      let stayTime = smaUtils.getTime() - beginTime
-      SMA.track({ event: eventMap.pageView, stayTime, scrollTop: SMA.Data.scrollTop, route: currRoute })
-      SMA.Data.scrollTop = 0
-      oOnHide && oOnHide.call(this, arguments)
-      // 是否在onHide里发送数据
-      smaUtils.request()
-      // smaUtils.request('errorData')
-    }
-
-    // onShare
-    if (smConfig.statShareApp && args[0].onShareAppMessage) {
-      const oOnShareAppMessage = args[0].onShareAppMessage
-      args[0].onShareAppMessage = function () {
-        console.log('用户分享...', arguments)
-        currRoute = smaUtils.getPagePath()
-        SMA.track({ event: eventMap.wxShare, route: currRoute })
-        oOnShareAppMessage.call(this, arguments)
+function autoTrack () {
+  let beginTime = 0,
+    currRoute = ''
+  function e(t, n, o) {
+    if (t[n]) {
+      let e = t[n]
+      t[n] = function (t) {
+        // 原始方法调用
+        e.call(this, t)
+        // 用户添加方法调用
+        o.call(this, t, n)
+      }
+    } else {
+      t[n] = function (t) {
+        o.call(this, t, n)
       }
     }
-
-     // onPullDownRefresh
-    if (smConfig.statPullDown && args[0].onPullDownRefresh) {
-      const oOnPullDownRefresh = args[0].onPullDownRefresh
-      args[0].onPullDownRefresh = function () {
-        currRoute = smaUtils.getPagePath()
-        console.log('用户下拉刷新...', arguments, currRoute)
-        SMA.track({ event: eventMap.pullDownRefresh, route: currRoute })
-        // 发送分享信息
-        oOnPullDownRefresh.call(this, arguments)
-      }
-    }
-
-    // onPageScroll
-    if (smConfig.statPageScroll && args[0].onPageScroll) {
-      const oOnPageScroll = args[0].onPageScroll
-      args[0].onPageScroll = function(e) {
-        console.log('页面滚动:', e, e.scrollTop)
-        // 只记录页面滚动历史中的最大值
-        if (e.scrollTop > SMA.Data.pageScrollTop) {
-          SMA.Data.pageScrollTop = e.scrollTop
-        }
-        oOnPageScroll.call(this, arguments)
-      }
-    }
-
-    oPage.apply(null, [].slice.call(arguments))
   }
-
-  // 重写APP
+  function appShow(pram) {
+    smaUtils.logInfo('拦截的appOnShow.....')
+    beginTime = smaUtils.getTime()
+  }
+  function appHide(pram) {
+    let stayTime = (smaUtils.getTime() - beginTime).toFixed(1)
+    smaUtils.logInfo('appOnHide.....', stayTime)
+  }
+  function appLaunch(para) {
+    // 统计启动时的场景值
+    const scene = para.scene || -1
+    smaUtils.logInfo('appLaunch...场景值', scene)
+  }
+  function appError(e) {
+    const currRoute = smaUtils.getPagePath()
+    const data = { event: eventMap.onError, route: currRoute, errrMsg: e }
+    smaUtils.logInfo('拦截的onError', data)
+  }
+  function onLoad(para) {
+    smaUtils.logInfo('pageOnLoad', para)
+  }
+  function onShow(para, n) {
+    smaUtils.logInfo('拦截的onShow.....')
+    beginTime = smaUtils.getTime()
+  }
+  function onHide(para) {
+    currRoute = smaUtils.getPagePath()
+    let stayTime = smaUtils.getTime() - beginTime
+    SMA.track({ event: eventMap.pageView, stayTime, scrollTop: SMA.Data.scrollTop, route: currRoute })
+    SMA.Data.scrollTop = 0
+    smaUtils.logInfo('拦截的onHide....stayTime', stayTime, wx.smTracker)
+    // 是否在onHide里发送数据
+    smaUtils.request()
+  }
+  function onPullDownRefresh(para) {
+    currRoute = smaUtils.getPagePath()
+    smaUtils.logInfo('用户下拉刷新...', arguments, currRoute)
+    SMA.track({ event: eventMap.pullDownRefresh, route: currRoute })
+    // 发送下拉舒心信息
+  }
+  function onReachBottom() {
+    currRoute = smaUtils.getPagePath()
+    smaUtils.logInfo('下拉接触到底部...', arguments, currRoute)
+    SMA.track({ event: 'reachBottom', route: currRoute })
+  }
+  function onShareAppMessage(para) {
+    smaUtils.logInfo('用户分享...', arguments)
+    currRoute = smaUtils.getPagePath()
+    SMA.track({ event: eventMap.wxShare, route: currRoute })
+  }
+  function onPageScroll(e) {
+    smaUtils.logInfo('页面滚动:', e, e.scrollTop)
+    // 只记录页面滚动历史中的最大值
+    if (e.scrollTop > SMA.Data.pageScrollTop) {
+      SMA.Data.pageScrollTop = e.scrollTop
+    }
+  }
   /* global App */
-  let oApp = App
+  const oldApp = App
   // eslint-disable-next-line
-  App = function () {
-    let args = arguments
+  App = function (t) {
+    smaUtils.logInfo('APP:', t)
+    e(t, 'onLaunch', appLaunch)
+    e(t, 'onShow', appShow)
+    e(t, 'onHide', appHide)
     if (smConfig.errorReport) {
-      let oOnError = args[0].onError
-      args[0].onError = function () {
-        console.log('onError.....')
-        // 此类onError 即时上报
-        const currRoute = smaUtils.getPagePath()
-        console.log('拦截的onError', arguments[0])
-        SMA.report({ event: eventMap.onError, route: currRoute, errrMsg: arguments[0] })
-        oOnError && oOnError.apply(this, [].slice.call(arguments))
-      }
-
-      // 拦截console.warn, console.error
-      if (console) {
-        const oldWarn = console.warn,
-          oldError = console.error
-        console.warn = function() {
-          const e = [].slice.call(arguments)
-          if (!e.length) { return true }
-          const currRoute = smaUtils.getPagePath()
-          SMA.report({ event: eventMap.onWarn, route: currRoute, errrMsg: arguments[0] })
-          // 上传警告
-          console.log('捕捉到warn 事件,', e)
-          oldWarn.apply(console, e)
+      e(t, 'onError', appError)
+    }
+    oldApp(t)
+  }
+  /* global Page */
+  const oldPage = Page
+  // eslint-disable-next-line
+  Page = function(option) {
+    e(option, 'onLoad', onLoad)
+    e(option, 'onShow', onShow)
+    e(option, 'onHide', onHide)
+    option.onPullDownRefresh && e(option, 'onPullDownRefresh', onPullDownRefresh)
+    option.onReachBottom && e(option, 'onReachBottom', onReachBottom)
+    option.onShareAppMessage && e(option, 'onShareAppMessage', onShareAppMessage)
+    option.onPageScroll && e(option, 'onPageScroll', onPageScroll)
+    oldPage.apply(null, [].slice.call(arguments))
+  }
+  // 注入 SMA
+  wx.smTracker = SMA
+}
+// 错误和接口性能上报初始化
+function otherReportInit() {
+  // 错误上报 拦截console.warn, console.error
+  if (console && smConfig.errorReport) {
+    const oldWarn = console.warn,
+      oldError = console.error
+    console.warn = function() {
+      const e = [].slice.call(arguments)
+      if (!e.length) { return true }
+      const currRoute = smaUtils.getPagePath()
+      // SMA.report({ event: eventMap.onWarn, route: currRoute, errrMsg: arguments[0] })
+      // 上传警告
+      smaUtils.logInfo('捕捉到warn 事件,', e)
+      oldWarn.apply(console, e)
+    }
+    console.error = function() {
+      var e = [].slice.call(arguments)
+      if (!e.length) { return true }
+      const currRoute = smaUtils.getPagePath()
+      // SMA.report({ event: eventMap.onError, route: currRoute, errrMsg: arguments[0] })
+      smaUtils.logInfo('捕捉到error 事件,', e)
+      oldError.apply(console, e)
+    }
+  }
+  // 请求上报
+  if (smConfig.requestReport) {
+    let Request = {
+      request: function (e) {
+        let success = e[0].success,
+          fail = e[0].fail,
+          beginTime = smaUtils.getTime(),
+          endTime = 0
+        e[0].success = function () {
+          endTime = smaUtils.getTime()
+          const performance = {
+            event: eventMap.wxRequest,
+            url: e[0].url,
+            status: arguments[0].statusCode,
+            begin: beginTime,
+            end: endTime,
+            total: endTime - beginTime
+          }
+          smaUtils.logInfo('success performance:', performance)
+          SMA.track(performance)
+          success && success.apply(this, [].slice.call(arguments))
         }
-        console.error = function() {
-          var e = [].slice.call(arguments)
-          if (!e.length) { return true }
-          const currRoute = smaUtils.getPagePath()
-          SMA.report({ event: eventMap.onError, route: currRoute, errrMsg: arguments[0] })
-          console.log('捕捉到error 事件,', e)
-          oldError.apply(console, e)
+        e[0].fail = function () {
+          endTime = smaUtils.getTime()
+          const performance = {
+            event: eventMap.wxRequest,
+            url: e[0].url,
+            status: arguments[0].statusCode,
+            begin: beginTime,
+            end: endTime,
+            total: endTime - beginTime
+          }
+          smaUtils.logInfo('fail performance:', performance)
+          SMA.track(performance)
+          fail && fail.apply(this, [].slice.call(arguments))
+        }
+      },
+      downloadFile: function (e) {
+        // wx.downloadFile
+        let success = e[0].success,
+          fail = e[0].fail,
+          beginTime = smaUtils.getTime(),
+          endTime = 0
+        e[0].success = function (e) {
+          endTime = smaUtils.getTime()
+          const reportData = {
+            type: eventMap.downloadFile,
+            url: e[0].url,
+            total: endTime - beginTime
+          }
+          SMA.track(reportData)
+          smaUtils.logInfo('下载成功：', e, reportData)
+          success && success.apply(this, [].slice.call(arguments))
+        }
+        e[0].fail = function (e) {
+          const reportData = {
+            type: eventMap.downloadFile,
+            url: e[0].url,
+            desc: 'fail'
+          }
+          SMA.track(reportData)
+          smaUtils.logInfo('下载失败：', e, reportData)
+          fail && fail.apply(this, [].slice.call(arguments))
         }
       }
     }
-    if (smConfig.requestReport) {
-      let Request = {
-        request: function (e) {
-          let success = e[0].success,
-            fail = e[0].fail,
-            beginTime = smaUtils.getTime(),
-            endTime = 0
-          e[0].success = function () {
-            endTime = smaUtils.getTime()
-            const performance = {
-              event: eventMap.wxRequest,
-              url: e[0].url,
-              status: arguments[0].statusCode,
-              begin: beginTime,
-              end: endTime,
-              total: endTime - beginTime
-            }
-            console.log('success performance:', performance)
-            SMA.track(performance)
-            success && success.apply(this, [].slice.call(arguments))
-          }
-          e[0].fail = function () {
-            console.log('请求失败：', arguments)
-            endTime = smaUtils.getTime()
-            const performance = {
-              event: eventMap.wxRequest,
-              url: e[0].url,
-              status: arguments[0].statusCode,
-              begin: beginTime,
-              end: endTime,
-              total: endTime - beginTime
-            }
-            console.log('fail performance:', performance)
-            SMA.track(performance)
-            fail && fail.apply(this, [].slice.call(arguments))
-          }
-        },
-        downloadFile: function (e) {
-          // wx.downloadFile
-          let success = e[0].success,
-            fail = e[0].fail,
-            beginTime = smaUtils.getTime(),
-            endTime = 0
-          e[0].success = function (e) {
-            endTime = smaUtils.getTime()
-            const reportData = {
-              type: eventMap.downloadFile,
-              url: e[0].url,
-              total: endTime - beginTime
-            }
-            SMA.track(reportData)
-            console.log('下载成功：', e, reportData)
-            success && success.apply(this, [].slice.call(arguments))
-          }
-          e[0].fail = function (e) {
-            const reportData = {
-              type: eventMap.downloadFile,
-              url: e[0].url,
-              desc: 'fail'
-            }
-            SMA.track(reportData)
-            console.log('下载失败：', e, reportData)
-            fail && fail.apply(this, [].slice.call(arguments))
-          }
-        }
-      }
 
-      // 替换微信相关属性
-      if (!wx.smTracker) {
-        let oldWx = wx,
-          newWx = {}
-        for (var p in wx) {
-          if (Request[p]) {
-            let p2 = p.toString()
-            newWx[p2] = function () {
-              Request[p2](arguments)
-              oldWx[p2].apply(oldWx, [].slice.call(arguments))
-            }
-          } else {
-            newWx[p] = oldWx[p]
-          }
+    // 替换微信相关属性
+    let oldWx = wx,
+      newWx = {}
+    for (var p in wx) {
+      if (Request[p]) {
+        let p2 = p.toString()
+        newWx[p2] = function () {
+          Request[p2](arguments)
+          oldWx[p2].apply(oldWx, [].slice.call(arguments))
         }
-        newWx.smTracker = true
-        // eslint-disable-next-line
-        wx = newWx
       } else {
-        smaUtils.log(1001)
+        newWx[p] = oldWx[p]
       }
     }
-    // 注入 SMA
-    args[0].SMA = SMA
-    console.log('args:', args)
-    oApp.apply(null, [].slice.call(arguments))
+    // eslint-disable-next-line
+    wx = newWx
+  } else {
+    smaUtils.log(1001)
   }
 }
-
 const SMA = {
   init: function(config = {}) {
     if (wx.smTracker) {
@@ -345,8 +363,8 @@ const SMA = {
     }
     smConfig = Object.assign({}, smConfig, config)
     smaUtils.setBasicInfo()
-    // 拦截Page, App 全局方法
-    rePlacePage()
+    smConfig.autoTrack && autoTrack()
+    otherReportInit()
     // 定时发送？
     // SMA.Data.reportTimer = setInterval(() => {
     //   console.log('this.SMA.Data.staData:', this.Data.staData)
@@ -357,15 +375,14 @@ const SMA = {
   },
   // 事件跟踪
   track: function(data) {
-    console.log('数据跟踪.....', data)
+    smaUtils.logInfo('数据跟踪.....', data)
     SMA.Data.staData.push(smaUtils.composeData('track', data))
   },
   // 错误上报
   report: function(data) {
     // 这里为什么死循环了
-    console.log('错误上报。。。。1')
+    smaUtils.logInfo('错误上报。。。。1')
     SMA.Data.errorData.push(smaUtils.composeData('report', data))
-    console.log('错误上报。。。。2')
     smaUtils.request('errorData')
   },
   Data: {
