0c2a9a81456a6523dc55994b10b33cbf75a92493
zack-zhong <zhongdonling@gmail.com>
2020-08-02 11:29:01 +0800

    feat: 添加层次遍历代码

--- arithmetic/tree/binary_search_tree.js
+++ arithmetic/tree/binary_search_tree.js
@@ -168,3 +168,66 @@ function BFS_Traverse(node) {
 }
 console.log('-------------广度遍历------------')
 BFS_Traverse(bsOne.root)
+
+// 层次遍历，输出 二维数组：
+/**
+ * [
+    [23],
+    [16, 25],
+    [3, 22, 37, 99]
+   ]
+  * 时间复杂度： O(n)
+  * 空间复杂度： O(n)
+ */
+// 方法 1: 
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
+var levelOrder = function(root) {
+  var queue = []
+  var number = []
+  if (!root) return number
+
+  queue.push(root)
+  while(queue.length > 0) {
+    var currentLevelSize = queue.length
+    var currentLevelArr = []
+    for (var i = 0; i < currentLevelSize; i++) {
+      var node = queue.shift()
+      currentLevelArr.push(node.data)
+      if (node.left !== null) {
+        queue.push(node.left)
+      }
+      if (node.right !== null) {
+        queue.push(node.right)
+      }
+    }
+    // 将一层的数据添加到 结果数组里
+    number.push(currentLevelArr)
+  }
+  return number
+};
+
+var resLevelOrder = levelOrder(bsOne.root)
+console.log('层次遍历，输出二维数组1：', resLevelOrder)
+
+
+/*
+* desc: 层次遍历方法2：
+*/
+function levelOrder2(root) {
+  var number = [[]]
+  myPreOrder(root, 0, number)
+  return number
+}
+
+function myPreOrder(node, depth, number) {
+  if (!node) return
+  if (depth >= number.length ) {
+    number.push([])
+  }
+  number[depth].push(node.data)
+  myPreOrder(node.left, depth + 1, number)
+  myPreOrder(node.right, depth + 1, number)
+}
+var myPreOrderRes = levelOrder2(bsOne.root)
+
+console.log('层次遍历，输出二维数组2：', myPreOrderRes)
