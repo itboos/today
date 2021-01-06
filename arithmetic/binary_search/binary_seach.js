88ffe02323991a14e0b293605d4d0dec12f008e3
itboos <zhongdonling@gmail.com>
2021-01-06 11:06:50 +0800

    feat:更新文档

--- arithmetic/binary_search/binary_seach.js
+++ arithmetic/binary_search/binary_seach.js
@@ -8,20 +8,20 @@
 
 // 1.非递归实现
 /**
- * 
- * @param {Array} arr 
- * @param {*} value 
+ *
+ * @param {Array} arr
+ * @param {*} value
  * @returns {Number} index
  */
 function binarySearch(arr, value) {
   var low = 0,
-      high = arr.length - 1,
-      mid = 0;
-  var count = 0
-  while(low <= high) {
+    high = arr.length - 1,
+    mid = 0;
+  var count = 0;
+  while (low <= high) {
     mid = Math.floor((high + low) / 2);
-    count ++
-    console.log('第 ' + count + ' 次查找,' + ' mid=' + mid);
+    count++;
+    console.log("第 " + count + " 次查找," + " mid=" + mid);
     if (arr[mid] === value) return mid;
     if (arr[mid] > value) {
       high = mid - 1;
@@ -33,7 +33,7 @@ function binarySearch(arr, value) {
   return -1;
 }
 
-var arr = [0,1,2,3,4,5,6,7,8,9,10, 11]
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 // console.log(binarySearch(arr, 7)); // 7
 
 // 2. 递归实现
@@ -41,14 +41,15 @@ var arr = [0,1,2,3,4,5,6,7,8,9,10, 11]
 // 写递归思路: 1、找到递归公式，2、找到递归终止条件
 
 function binarySearch2(arr, low, high, value) {
-  var  mid =  Math.floor((high + low) / 2);
+  var mid = Math.floor((high + low) / 2);
 
-  // 递归终止条件
+  // 递归终止条件 1
   if (low > high) {
-    return -1
+    return -1;
   }
+  // 递归终止条件 2
   if (arr[mid] === value) {
-    return mid
+    return mid;
   }
 
   if (arr[mid] > value) {
@@ -59,33 +60,33 @@ function binarySearch2(arr, low, high, value) {
   return binarySearch2(arr, mid + 1, high, value);
 }
 
-var res = binarySearch2(arr, 0, arr.length-1, 1);
+var res = binarySearch2(arr, 0, arr.length - 1, 1);
 // console.log('res:', res);
 
-// 题目： 编程实现求一个数的平方根，要求精确到小数点后 6 位 
+// 题目： 编程实现求一个数的平方根，要求精确到小数点后 6 位
 // 思路： 用二分查找的方式求平方根
 function getSquare(x) {
   var low = 0,
-      mid = x/2,
-      high = x;
+    mid = x / 2,
+    high = x;
   var count = 0;
-  while(Math.abs((Math.pow(mid,2) - x)) > 0.000001) {
+  while (Math.abs(Math.pow(mid, 2) - x) > 0.000001) {
     // console.log('xxx', Math.abs((Math.pow(mid,2) - x)))
-    count++
-    if (Math.pow(mid,2) < x) {
-      low = mid
+    count++;
+    if (Math.pow(mid, 2) < x) {
+      low = mid;
     } else {
-      high = mid
+      high = mid;
     }
     mid = (low + high) / 2;
   }
   // console.log('mid', mid)
   // return mid.toFixed(6);
-  console.log('总查找次数：', count);
+  console.log("总查找次数：", count);
   return mid;
 }
 
 // console.log('getSquare(4)', getSquare(4))
 // console.log('getSquare(4)', getSquare(9)) // 3.000000089406967 js 中会有精度的问题
-console.log('getSquare(4)', getSquare(8 * Math.pow(10, 9)))
+console.log("getSquare(4)", getSquare(8 * Math.pow(10, 9)));
 // 经过测试，查找 8亿多 的平方根，大概只查找了 68 次，效率还是很高的。
